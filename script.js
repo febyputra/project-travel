40c343274163429c419c26de87b5e392fcefad73
febyputra <93692256+febyputra@users.noreply.github.com>
2023-05-23 13:49:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -37,16 +37,39 @@ var swiper = new Swiper(".reviews-slider", {
    },
 });
 
-let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+const loadMoreBtn = document.querySelector(".packages .load-more .btn");
 let currentItem = 3;
 
-loadMoreBtn.onclick = () =>{
-   let boxes = [document.querySelectorAll('.packages .box-container .box')];
-   for (var i = currentItem; i < currentItem + 3; i++){
-      boxes[i].style.display = 'inline-block';
-   };
-   currentItem += 3;
-   if(currentItem >= boxes.length){
-      loadMoreBtn.style.display = 'none';
+loadMoreBtn.onclick = () => {
+   const boxes = document.querySelectorAll(".packages .box-container .box");
+
+   for (let i = currentItem; i < currentItem + 3; i++) {
+      boxes[i].style.display = "inline-block";
+
+      currentItem += 3;
+      if (currentItem >= boxes.length) {
+         loadMoreBtn.style.display = "none";
+      }
    }
+}
+
+function whatsapp () {
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var phone = document.getElementById("phone").value;
+var address = document.getElementById("address").value;
+var location = document.getElementById("location").value;
+var guests = document.getElementById("guests").value;
+var date = document.getElementById("date").value;
+
+var url = "https://wa.me/6287865430699?text="
++"Name : "+name+"%0a"
++"Email : "+email+"%0a"
++"Phone : "+phone+"%0a"
++"Address : "+address+"%0a"
++"Location : "+location+"%0a"
++"Guests : "+guests+"%0a"
++"Date : "+date;
+
+window.open(url, '_blank').focus();
 }
