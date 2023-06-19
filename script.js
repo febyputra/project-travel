b6b9e7be0b2cc01070d4457fcdaec7ae7f6d4175
febyputra <93692256+febyputra@users.noreply.github.com>
2023-06-19 08:16:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -40,36 +40,5 @@ var swiper = new Swiper(".reviews-slider", {
 const loadMoreBtn = document.querySelector(".packages .load-more .btn");
 let currentItem = 3;
 
-loadMoreBtn.onclick = () => {
-   const boxes = document.querySelectorAll(".packages .box-container .box");
 
-   for (let i = currentItem; i < currentItem + 3; i++) {
-      boxes[i].style.display = "inline-block";
 
-      currentItem += 3;
-      if (currentItem >= boxes.length) {
-         loadMoreBtn.style.display = "none";
-      }
-   }
-}
-
-function whatsapp () {
-var name = document.getElementById("name").value;
-var email = document.getElementById("email").value;
-var phone = document.getElementById("phone").value;
-var address = document.getElementById("address").value;
-var location = document.getElementById("location").value;
-var guests = document.getElementById("guests").value;
-var date = document.getElementById("date").value;
-
-var url = "https://wa.me/6287865430699?text="
-+"Name : "+name+"%0a"
-+"Email : "+email+"%0a"
-+"Phone : "+phone+"%0a"
-+"Address : "+address+"%0a"
-+"Location : "+location+"%0a"
-+"Guests : "+guests+"%0a"
-+"Date : "+date;
-
-window.open(url, '_blank').focus();
-}
